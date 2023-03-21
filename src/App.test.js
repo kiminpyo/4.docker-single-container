84177f97db8cc59624cfcaa85bb618ae50a76537
kiminpyo <rladlsvy2448@naver.com>
2023-03-22 00:35:42 +0900

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-
 test("renders learn react link", () => {
     render(<App />);
-    const linkElement = screen.getByText(/frfdfefsrds/i);
+    const linkElement = screen.getByText(/리액트 앱입니다/i);
     expect(linkElement).toBeInTheDocument();
 });
-
